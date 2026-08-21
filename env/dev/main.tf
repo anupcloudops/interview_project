efedafebdaee5ce54c0088fed1c4946025a1e631
anupcloudops <anupeworld@gmail.com>
2026-08-21 16:26:04 +0530

    3nd

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -2,3 +2,16 @@ module "rg" {
     source = "../../module/01_rg"
     rgs= var.rgs
 }
+
+resource "azurerm_resource_group" "rgb" {
+    count = 3
+    name = "rg-${count.index}"
+    location = "east us"
+tags = {
+    environment = "dev"
+    managed_by = "Anup"
+}
+lifecycle {
+  ignore_changes = [ tags ]
+}
+}
--- module/01_rg/main.tf
+++ module/01_rg/main.tf
@@ -7,15 +7,3 @@ resource "azurerm_resource_group" "rgblock" {
     }
 }
 
-resource "azurerm_resource_group" "rgb" {
-    count = 3
-    name = "rg-${count.index}"
-    location = "east us"
-tags = {
-    environment = "dev"
-    managed_by = "Anup"
-}
-lifecycle {
-  ignore_changes = [ tags ]
-}
-}
